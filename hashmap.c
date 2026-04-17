3bc0535c12b20e5e67abff7b288b7dc229276228
Your Name <you@example.com>
2026-04-17 16:02:29 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -133,7 +133,7 @@ void enlarge(HashMap * map) {
     Pair** old_buckets = map->buckets;
     long oldCapacity = map->capacity;
     map->capacity *= 2;
-    map->buckets = calloc(map->capacity, sizeof(Pair*));
+    map->buckets = calloc(map->capacity, sizeof(Pair));
     map->size = 0;
     for(long i = 0; i < oldCapacity; i++){
         insertMap(map, old_buckets[i]->key, old_buckets[i]->value);
